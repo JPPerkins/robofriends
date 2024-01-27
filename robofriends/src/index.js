f76aeb67cf6c7dbeaed848c75e39554a73cebd9c
JPPerkins <jcperkins482@gmail.com>
2024-01-26 21:04:01 -0500

    removed bootstrap, added tachyons.

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Card from './Card';
+import 'tachyons';
+import robots from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
