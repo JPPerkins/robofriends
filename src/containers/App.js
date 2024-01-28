5daa473e666507ce275e6bd543e62ed9447d209b
JPPerkins <jcperkins482@gmail.com>
2024-01-27 21:07:52 -0500

    Finished functionality with using Hooks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,15 +9,15 @@ function App() {
 	const [robots, setRobots] = useState([]);
 	const [searchfield, setSearchfield] = useState('');
 
-	// componentDidMount() {
-	// 	fetch('https://jsonplaceholder.typicode.com/users')
-	// 		.then(response => response.json())
-	// 		.then(users => this.setState({robots: users}));
-	// }
+	useEffect(() => {
+	 	fetch('https://jsonplaceholder.typicode.com/users')
+	 		.then(response => response.json())
+	 		.then(users => setRobots(users));
+	}, []);
 
 	const onSearchChange = (event) => {
 		setSearchfield(event.target.value);
-	}
+	};
 
 	const filteredRobots = robots.filter(robot => {
 		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
